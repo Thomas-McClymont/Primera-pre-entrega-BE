4e807e02176a781136c969be2f3d5b7bab3d056b
thomasmcclymont <th.mcclymont@gmail.com>
2023-08-08 17:53:00 -0300

    Correcciones primera pre entrega

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartRouter from "./routes/carts.router.js";
+import ProductManager from "./controllers/productManager.js";
+
+const product = new ProductManager()
 
 const app = express();
 const puerto = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 app.use("/api/products/", productsRouter);
 app.use("/api/carts/", cartRouter);
+
+app.post("/products", async (req, res) => {
+    let newProduct = req.body
+    res.send(await product.writeProducts(newProduct))
+})
+
 app.listen(puerto, () => {
     console.log(`Servidor corriendo en el puerto ${puerto}`);
 })
--- src/controllers/productManager.js
+++ src/controllers/productManager.js
@@ -5,12 +5,15 @@ class ProductManager {
     constructor(){
         this.path = "./src/models/products.json"
     }
-    writeProducts = async (product) => {
-        await fs.writeFile(this.path, JSON.stringify(product))
-    }
     readProducts = async () => {
         let products = await fs.readFile(this.path, "utf-8")
         return JSON.parse(products)
+    }
+    writeProducts = async (product) => {
+        let productsOld = await this.readProducts()
+        let productAll = [...productsOld, product]
+        await fs.writeFile(this.path,JSON.stringify(productAll))
+        return "Product added";
     }
     exist = async (id) => {
         let products = await this.readProducts()
